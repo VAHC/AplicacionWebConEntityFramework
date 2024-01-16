2f16189def80276ae25452cb5a2821d72da771d1
VAHC <victor10alha@gmail.com>
2024-01-16 18:32:53 -0300

    Formulario Cliente

--- MiPrimeraAplicacionWebConEntityFramework/Controllers/ClienteController.cs
+++ MiPrimeraAplicacionWebConEntityFramework/Controllers/ClienteController.cs
@@ -27,5 +27,55 @@ namespace MiPrimeraAplicacionWebConEntityFramework.Controllers
             }
             return View(listaCliente);
         }
+
+        List<SelectListItem> listaSexo;
+
+        private void llenarSexo()
+        {
+            using(var bd=new BDPasajeEntities())
+            {
+                listaSexo = (from sexo in bd.Sexo
+                             where sexo.BHABILITADO == 1
+                             select new SelectListItem
+                             {
+                                 Text = sexo.NOMBRE,
+                                 Value = sexo.IIDSEXO.ToString()
+                             }).ToList();
+                listaSexo.Insert(0, new SelectListItem { Text = "--Seleccione--", Value = "" });
+            }
+        }
+        public ActionResult Agregar()
+        {
+            llenarSexo();
+            ViewBag.lista = listaSexo;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(ClienteCLS oClienteCLS)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(oClienteCLS);
+            }
+            using (var bd = new BDPasajeEntities())
+            {
+                Cliente oCliente = new Cliente();
+                oCliente.NOMBRE = oClienteCLS.nombre;
+                oCliente.APPATERNO = oClienteCLS.apPaterno;
+                oCliente.APMATERNO = oClienteCLS.apMaterno;
+                oCliente.EMAIL = oClienteCLS.email;
+                oCliente.DIRECCION = oClienteCLS.direccion;
+                oCliente.DIRECCION = oClienteCLS.direccion;
+                oCliente.IIDSEXO = oClienteCLS.iidsexo;
+                oCliente.TELEFONOFIJO = oClienteCLS.telefonoFijo;
+                oCliente.TELEFONOCELULAR = oClienteCLS.telefonoCelular;
+                oCliente.BHABILITADO = 1;
+                bd.Cliente.Add(oCliente);
+                bd.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- MiPrimeraAplicacionWebConEntityFramework/Models/ClienteCLS.cs
+++ MiPrimeraAplicacionWebConEntityFramework/Models/ClienteCLS.cs
@@ -12,19 +12,43 @@ namespace MiPrimeraAplicacionWebConEntityFramework.Models
         public int iidcliente { get; set; }
 
         [Display(Name = "Nombre Cliente")]
+        [StringLength(100, ErrorMessage ="Longitud maxima 100")]
+        [Required]
         public string nombre { get; set; }
 
         [Display(Name = "Apellido Paterno")]
+        [StringLength(150, ErrorMessage = "Longitud maxima 150")]
+        [Required]
         public string apPaterno { get; set; }
 
         [Display(Name = "Apellido Materno")]
+        [StringLength(150, ErrorMessage = "Longitud maxima 150")]
+        [Required]
         public string apMaterno { get; set;}
+        
+        [Display(Name ="Email")]
+        [StringLength(200, ErrorMessage = "Longitud maxima 200")]
+        [EmailAddress(ErrorMessage ="Ingrese un email valido")]
+        [Required]
         public string email { get; set; }
+        
+        [Display(Name ="Direccion")]
+        [StringLength(200, ErrorMessage = "Longitud maxima 200")]
+        [Required]
         public string direccion { get; set; }
+
+        [Display(Name = "Sexo")]
+        [Required]
         public int iidsexo { get; set; }
 
         [Display(Name = "Telefono Fijo")]
+        [StringLength(10, ErrorMessage = "Longitud maxima 10")]
+        [Required]
         public string telefonoFijo { get; set; }
+
+        [Display(Name = "Telefono Celular")]
+        [StringLength(10, ErrorMessage = "Longitud maxima 10")]
+        [Required]
         public string telefonoCelular { get; set; }
         public int bhabilitado { get; set; }
     }
