396c17ebee7b05190096de2ab4c1e1a5c5ecb2fb
VAHC <victor10alha@gmail.com>
2024-02-11 12:03:51 -0300

    Alert para Editar y Agregar

--- MiPrimeraAplicacionWebConEntityFramework/Controllers/MarcaController.cs
+++ MiPrimeraAplicacionWebConEntityFramework/Controllers/MarcaController.cs
@@ -31,12 +31,12 @@ namespace MiPrimeraAplicacionWebConEntityFramework.Controllers
             return View();    
         }
 
-        public ActionResult Editar(int ed)
+        public ActionResult Editar(int uf)
         {
             MarcaCLS oMarcaCLS = new MarcaCLS();
             using(var bd=new BDPasajeEntities())
             {
-                Marca oMarca = bd.Marca.Where(p => p.IIDMARCA.Equals(ed)).First();
+                Marca oMarca = bd.Marca.Where(p => p.IIDMARCA.Equals(uf)).First();
                 oMarcaCLS.iidmarca = oMarca.IIDMARCA;
                 oMarcaCLS.nombre = oMarca.NOMBRE;
                 oMarcaCLS.descripcion = oMarca.DESCRIPCION;
@@ -44,6 +44,27 @@ namespace MiPrimeraAplicacionWebConEntityFramework.Controllers
             return View(oMarcaCLS);
         }
 
+        [HttpPost]
+        public ActionResult Editar(MarcaCLS oMarcaCLS)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(oMarcaCLS);
+            }
+            
+
+            int idMarca = oMarcaCLS.iidmarca;
+            using(var bd=new BDPasajeEntities())
+            {                
+                Marca oMarca = bd.Marca.Where(p => p.IIDMARCA.Equals(idMarca)).First();
+                oMarca.NOMBRE = oMarcaCLS.nombre;
+                oMarca.DESCRIPCION = oMarcaCLS.descripcion;
+                bd.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+            
         //Hace la inserción
         [HttpPost]
         public ActionResult Agregar(MarcaCLS oMarcaCLS)
