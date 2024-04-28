28688c9c35199dc041d39628149ce7ea9c619c90
VAHC <victor10alha@gmail.com>
2024-04-28 18:55:59 -0300

    Ajax BeginForm POST

--- MiPrimeraAplicacionWebConEntityFramework/Controllers/RolController.cs
+++ MiPrimeraAplicacionWebConEntityFramework/Controllers/RolController.cs
@@ -25,13 +25,15 @@ namespace MiPrimeraAplicacionWebConEntityFramework.Controllers
             }
             return View(listaRol);
         }
+
         public ActionResult Filtro(string nombre)
         {
+
             List<RolCLS> listaRol = new List<RolCLS>();
             using (var bd = new BDPasajeEntities())
             {
-                if (nombre == null)
-                    listaRol = (from rol in bd.Rol
+                if(nombre==null)
+                listaRol = (from rol in bd.Rol
                             where rol.BHABILITADO == 1
                             select new RolCLS
                             {
@@ -51,6 +53,25 @@ namespace MiPrimeraAplicacionWebConEntityFramework.Controllers
                                 }).ToList();
             }
             return PartialView("_TablaRol", listaRol);
+        }
+
+        public int Guardar(RolCLS oRolCLS,int titulo)
+        {
+            int rpta = 0;
+            using(var bd=new BDPasajeEntities())
+            {
+                if (titulo.Equals(1))
+                {
+                    Rol oRol = new Rol();
+                    oRol.NOMBRE = oRolCLS.nombre;
+                    oRol.DESCRIPCION = oRolCLS.descripcion;
+                    oRol.BHABILITADO = 1;
+                    bd.Rol.Add(oRol);
+                    rpta= bd.SaveChanges();
+                }
+            }
+
+            return rpta;
         }
     }
 }
