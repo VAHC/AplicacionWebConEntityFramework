9d337bcd68e997d3f99bb3bf60d03b391872b728
VAHC <victor10alha@gmail.com>
2024-04-27 21:00:11 -0300

    Agrego modelos y controladores Usuario, Rol y Pagina

---
+++ MiPrimeraAplicacionWebConEntityFramework/Models/RolCLS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimeraAplicacionWebConEntityFramework.Models
+{
+    public class RolCLS
+    {
+        [Display(Name="id Rol")]
+        public int iidRol { get; set; }
+        [Required]
+        [Display(Name = "Nombre Rol")]
+        public string nombre { get; set; }
+        [Required]
+        [Display(Name = "Descripción Rol")]
+        public string descripcion { get; set; }
+        public int bhabilitado { get; set; }
+    }
+}
