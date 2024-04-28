34a920e37f11d45af1e3e74df98deb21b4543c9d
VAHC <victor10alha@gmail.com>
2024-04-27 22:58:25 -0300

    Vistas parciales para filtros

--- MiPrimeraAplicacionWebConEntityFramework/Controllers/RolController.cs
+++ MiPrimeraAplicacionWebConEntityFramework/Controllers/RolController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MiPrimeraAplicacionWebConEntityFramework.Models;
 namespace MiPrimeraAplicacionWebConEntityFramework.Controllers
 {
     public class RolController : Controller
@@ -11,7 +11,46 @@ namespace MiPrimeraAplicacionWebConEntityFramework.Controllers
         // GET: Rol
         public ActionResult Index()
         {
-            return View();
+            List<RolCLS> listaRol = new List<RolCLS>();
+            using(var bd=new BDPasajeEntities())
+            {
+                listaRol = (from rol in bd.Rol
+                            where rol.BHABILITADO == 1
+                            select new RolCLS
+                            {
+                                iidRol = rol.IIDROL,
+                                nombre = rol.NOMBRE,
+                                descripcion = rol.DESCRIPCION
+                            }).ToList();
+            }
+            return View(listaRol);
+        }
+        public ActionResult Filtro(string nombre)
+        {
+            List<RolCLS> listaRol = new List<RolCLS>();
+            using (var bd = new BDPasajeEntities())
+            {
+                if (nombre == null)
+                    listaRol = (from rol in bd.Rol
+                            where rol.BHABILITADO == 1
+                            select new RolCLS
+                            {
+                                iidRol = rol.IIDROL,
+                                nombre = rol.NOMBRE,
+                                descripcion = rol.DESCRIPCION
+                            }).ToList();
+                else
+                    listaRol = (from rol in bd.Rol
+                                where rol.BHABILITADO == 1
+                                && rol.NOMBRE.Contains(nombre)
+                                select new RolCLS
+                                {
+                                    iidRol = rol.IIDROL,
+                                    nombre = rol.NOMBRE,
+                                    descripcion = rol.DESCRIPCION
+                                }).ToList();
+            }
+            return PartialView("_TablaRol", listaRol);
         }
     }
 }
